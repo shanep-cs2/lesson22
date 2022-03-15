5086b96069a74dd0f529b978f479cb62d02e65fa
Shane K. Panter <shane@foundationcode.com>
2022-03-14 18:02:42 -0600

    Update BinarySearch.java

--- app/src/main/java/lesson/BinarySearch.java
+++ app/src/main/java/lesson/BinarySearch.java
@@ -8,8 +8,8 @@ public interface BinarySearch {
      * 
      * @param helper A populated search helper
      * @param nums An ordered list of numbers
-     * @param start The starting index
-     * @param end The ending index
+     * @param lower The starting index
+     * @param upper The ending index
      * @return A populated SearchHelper object
      */
     SearchHelper search(SearchHelper helper, List<Integer> nums, int lower, int upper);
